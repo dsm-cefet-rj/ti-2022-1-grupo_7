615023589c255c8ce597e9cbf12b023c5a4617b8
Victor Coculilo <74696760+VictorCoculilo@users.noreply.github.com>
2022-05-18 19:46:11 -0300

    Update Box.js

--- Reserva2/src/components/Box/Box.js
+++ Reserva2/src/components/Box/Box.js
@@ -1,14 +1,14 @@
 import './Box.css'
 
-function Box(){
+function Box({nomeProduto, foto, descProduto}){
 
     return(
                     <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
                         <div class="card borda ">
                             <div class="card-body ">
-                                <img class="w-100 " src="CalcaVerde.jpg " alt="Calça "/>
-                                <h2 class="mt-2 ">Item 1</h2>
-                                <p>Mt texto textinho ms piui piui Mt texto textinho ms piui piui Mt texto textinho ms piui piui Mt texto textinho ms piui piui Mt texto textinho ms piui piui </p>
+                                <img class="w-100 " src={foto} alt="Calça "/>
+                                <h2 class="mt-2 ">{nomeProduto}</h2>
+                                <p>{descProduto}</p>
                                 <button2 class="botao ">Comprar</button2>
                             </div>
                         </div>
