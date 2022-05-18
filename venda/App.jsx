d3bc7a792c49c9e2c5caf39cf75e776fe86d0f8b
JosePauloM19168 <104699228+JosePauloM19168@users.noreply.github.com>
2022-05-18 16:11:29 -0300

    Add files via upload

---
+++ venda/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import './App.css';
+import Produto from './Vender';
+
+const App = () => {
+  
+  const [tableData, setTableData] = useState([])
+  const [formInputData, setformInputData] = useState(
+      {
+      nomeProduto:'',
+      qtdProduto:''
+      }
+  );
+ 
+  const handleChange=(evnt)=>{  
+      const newInput = (data)=>({...data, [evnt.target.name]:evnt.target.value})
+      setformInputData(newInput)
+  }
+  
+  const handleSubmit= (evnt) =>{
+      evnt.preventDefault();
+      const checkEmptyInput = !Object.values(formInputData).every(res=>res==="")
+      if(checkEmptyInput)
+      {
+        const newData = (data)=>([...data, formInputData])
+        setTableData(newData);
+        const emptyInput= {nomeProduto:'', qtdProduto:''}
+        setformInputData(emptyInput)
+      }
+  }
+  
+  return(
+  <>
+     <main>
+    
+        <Produto handleChange={handleChange} formInputData={formInputData} handleSubmit={handleSubmit}/>
+
+      </main>
+  </>  
+   
+  )
+}
+
+export default App;
