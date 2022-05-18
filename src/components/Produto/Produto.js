348a0e5037cbe59926adc085cf150bb43ef25c86
Victor Coculilo <74696760+VictorCoculilo@users.noreply.github.com>
2022-05-18 16:36:58 -0300

    Update Produto.js

--- src/components/Produto/Produto.js
+++ src/components/Produto/Produto.js
@@ -1,6 +1,6 @@
 import './Produto.css'
 
-function Produto(){
+function Produto({foto, preco, nomeProduto, descProduto, tamanho, categoria, quantidade}){
     return(
         <main>
         <div class="container">
@@ -8,18 +8,17 @@ function Produto(){
                 <div class="card-body ">
                     <div class="row ">
                         <div class="oofset-2 col-sm-12 col-md-12 col-lg-5  box-esq">
-                            <img class="w-100 " src="CalcaVerde.jpg" alt="Calça "/>
-                            <p class="preco">R$ <span class="bold">249,99</span></p>
+                            <img class="w-100 " src={foto} alt="Calça "/>
+                            <p class="preco">R$ <span class="bold">{preco}</span></p>
 
                         </div>
                         <div class="col-sm-12 col-md-12 col-lg-5 box-dir">
-                            <h2 class="nome-produto">PRODUTO</h2>
+                            <h2 class="nome-produto">{nomeProduto}</h2>
                             <section class="descricao"/>
-                                <p class="categoria">descrição do produto aqui... muita descrição msm,descrição do produto aqui... muita descrição msm,descrição do produto aqui... muita descrição msm,descrição do produto aqui... muita descrição msm,descrição do produto aqui...
-                                    muita descrição msm</p>
-                                <p class="categoria">Tamanho: <b>P</b></p>
-                                <p class="categoria">Categoria: <b>Calça</b></p>
-                                <p class="categoria">Quantidade: <b>6</b></p>
+                                <p class="categoria" id="texto">{descProduto}</p>
+                                <p class="categoria">Tamanho: <b>{tamanho}</b></p>
+                                <p class="categoria">Categoria: <b>{categoria}</b></p>
+                                <p class="categoria">Quantidade: <b>{quantidade}</b></p>
                                 <a href="#" class="botao">Comprar</a>
                         </div>
                     </div>
