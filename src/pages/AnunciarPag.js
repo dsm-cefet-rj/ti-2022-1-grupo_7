07d8e99d85706470010af775be8d446bdeca69fe
Victor Coculilo <74696760+VictorCoculilo@users.noreply.github.com>
2022-05-25 18:34:42 -0300

    Update AnunciarPag.js

--- src/pages/AnunciarPag.js
+++ src/pages/AnunciarPag.js
@@ -1,34 +1,23 @@
-function Anunciar(){
+import './App.css';
+import Criar from './components/Produto/Criar';
+import React, { useState } from "react";
+import Header from './components/Produto/Header';
 
-    return(
-        <div>
-            <h1>Anunciar</h1>
-            <p>COLOCA SUA SEUS ELEMENTOS AQUI!</p>
-        </div>
-    )
+
+function Anunciar(){
+        return(
+  <>
+  
+    <Header/>
+   
+     <main>
+    
+        <Criar handleImputChange/>
+
+      </main>
+  </>  
+   
+  )
 }
 
-export default Anunciar
-
-//import './App.css';
-// import Criar from './components/Produto/Criar';
-//import React, { useState } from "react";
-//import Header from './components/Produto/Header';
-
-//const App = () => {
-//  return(
-  //<>
-  
-    //<Header/>
-    
-     //<main>
-    
-       // <Criar handleImputChange/>
-
-     // </main>
- // </>  
-   
-  //)
-//}
-
-//export default App;
+export default App;
