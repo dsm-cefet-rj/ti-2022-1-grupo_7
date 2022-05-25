05938671663d7284aee2f47fc3c38e9e1fc9ecb3
VictorCoculilo <victorcd00@gmail.com>
2022-05-25 01:05:25 -0300

    variaveis nas box

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,15 +1,15 @@
 import './Box.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-function Box(){
+function Box({foto, nomeProduto, descProduto}){
 
     return(
                     <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
                         <div className="card borda ">
                             <div className="card-body ">
-                                <img className="w-100 " src="CalcaVerde.jpg " alt="Calça "/>
-                                <h2 className="mt-2 ">Item 1</h2>
-                                <p>Mt texto textinho ms piui piui Mt texto textinho ms piui piui Mt texto textinho ms piui piui Mt texto textinho ms piui piui Mt texto textinho ms piui piui </p>
+                                <img className="w-100 " src={foto} alt="Roupa"/>
+                                <h2 className="mt-2 ">{nomeProduto}</h2>
+                                <p>{descProduto}</p>
                                 <Link className="botao" to="/ProdutoPag">Comprar</Link>
                             </div>
                         </div>
--- src/components/catalogo/Catalogo.js
+++ src/components/catalogo/Catalogo.js
@@ -6,14 +6,38 @@ function Catalogo(){
         <div>
             <div class="conteiner ">
                 <div class="row g-5 p-5 " id="estrutura">
-                    <Box/>
-                    <Box/>
-                    <Box/>
-                    <Box/>
-                    <Box/>
-                    <Box/>
-                    <Box/>
-                    <Box/>
+                    <Box
+                        foto ={'CalcaVerde.jpg'} 
+                        nomeProduto={'Calça Verde'} 
+                        descProduto={'Calça verde feita de tecido resistente a chuvas, fogo e até mesmo sujeira, usada como uniforme em exercitos russos.'}/>
+                    <Box
+                        foto ={'CalcaVerde.jpg'} 
+                        nomeProduto={'Calça Verde'} 
+                        descProduto={'Calça verde feita de tecido resistente a chuvas, fogo e até mesmo sujeira, usada como uniforme em exercitos russos.'}/>
+                    <Box
+                        foto ={'CalcaVerde.jpg'} 
+                        nomeProduto={'Calça Verde'} 
+                        descProduto={'Calça verde feita de tecido resistente a chuvas, fogo e até mesmo sujeira, usada como uniforme em exercitos russos.'}/>
+                    <Box
+                        foto ={'CalcaVerde.jpg'} 
+                        nomeProduto={'Calça Verde'} 
+                        descProduto={'Calça verde feita de tecido resistente a chuvas, fogo e até mesmo sujeira, usada como uniforme em exercitos russos.'}/>
+                    <Box
+                        foto ={'CalcaVerde.jpg'} 
+                        nomeProduto={'Calça Verde'} 
+                        descProduto={'Calça verde feita de tecido resistente a chuvas, fogo e até mesmo sujeira, usada como uniforme em exercitos russos.'}/>
+                    <Box
+                        foto ={'CalcaVerde.jpg'} 
+                        nomeProduto={'Calça Verde'} 
+                        descProduto={'Calça verde feita de tecido resistente a chuvas, fogo e até mesmo sujeira, usada como uniforme em exercitos russos.'}/>
+                    <Box
+                        foto ={'CalcaVerde.jpg'} 
+                        nomeProduto={'Calça Verde'} 
+                        descProduto={'Calça verde feita de tecido resistente a chuvas, fogo e até mesmo sujeira, usada como uniforme em exercitos russos.'}/>
+                    <Box
+                        foto ={'CalcaVerde.jpg'} 
+                        nomeProduto={'Calça Verde'} 
+                        descProduto={'Calça verde feita de tecido resistente a chuvas, fogo e até mesmo sujeira, usada como uniforme em exercitos russos.'}/>
                 </div>       
             </div>
         </div>
