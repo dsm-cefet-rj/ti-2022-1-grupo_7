9910cf19c4fa67614a92b74fa85a252185cc5aa0
VictorCoculilo <victorcd00@gmail.com>
2022-05-24 22:25:50 -0300

    teste

---
+++ src/components/catalogo/Catalogo.js
@@ -0,0 +1,23 @@
+import Box from "../Box/Box"
+import './catalogo.css'
+
+function Catalogo(){
+    return(
+        <div>
+            <div class="conteiner ">
+                <div class="row g-5 p-5 " id="estrutura">
+                    <Box/>
+                    <Box/>
+                    <Box/>
+                    <Box/>
+                    <Box/>
+                    <Box/>
+                    <Box/>
+                    <Box/>
+                </div>       
+            </div>
+        </div>
+    )
+}
+
+export default Catalogo
