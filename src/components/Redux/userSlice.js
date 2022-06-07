54988ef1372acbfcce2aae6060870caed43ad2cb
VictorCoculilo <victorcd00@gmail.com>
2022-06-06 21:54:27 -0300

    teste redux

---
+++ src/components/Redux/userSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const slice = createSlice({
+    name:'user',
+    intialState: {
+        name:'',
+        isLogged: false,
+    },
+    
+    reducers: {
+        changeUser(state, { payload }){
+            return {...state, isLogged: true, name: payload}
+        },
+
+        logout(state){
+            return {...state, isLogged: false, name:''}
+        }
+    }       
+})
+
+export const { changeUser, logout } = slice.actions
+
+export const selectUser = state => state.user
+
+export default slice.reducer
